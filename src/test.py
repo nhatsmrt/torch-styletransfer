1e1e92c2903ad72e53ffc51abecb7b3c54d29919
nhatsmrt <nhatsmrt@uw.edu>
2019-07-06 20:54:59 -0700

    Test using bigger resolution.

--- src/test.py
+++ src/test.py
@@ -76,17 +76,17 @@ def run_test_multiple(
 
     print("Begin creating dataset")
 
-    # content_images = UnlabelledImageDataset("MiniCOCO/256/")
-    # style_images = UnlabelledImageListDataset(style_path, transform=Compose(
-    #     [
-    #         Resize(512),
-    #         RandomCrop((256, 256))
-    #     ]
-    # ))
+    content_images = UnlabelledImageDataset("MiniCOCO/256/")
+    style_images = UnlabelledImageListDataset(style_path, transform=Compose(
+        [
+            Resize(512),
+            RandomCrop((256, 256))
+        ]
+    ))
 
-    img_dim = (128, 128)
-    content_images = UnlabelledImageDataset("MiniCOCO/128/", img_dim=img_dim)
-    style_images = UnlabelledImageDataset(style_path, img_dim=img_dim)
+    # img_dim = (128, 128)
+    # content_images = UnlabelledImageDataset("MiniCOCO/128/", img_dim=img_dim)
+    # style_images = UnlabelledImageDataset(style_path, img_dim=img_dim)
 
 
     # content_images = UnlabelledImageListDataset("data/", img_dim=img_dim)
