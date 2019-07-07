179b40b2f095d11f74d83520bf7ad30949de468a
nhatsmrt <nhatsmrt@uw.edu>
2019-07-06 19:48:54 -0700

    Switch back to 128 for now, and replace 3x3 with 1x1 kernel in resnet.

--- src/test.py
+++ src/test.py
@@ -75,19 +75,18 @@ def run_test_multiple(
     std = [0.229, 0.224, 0.225]
 
     print("Begin creating dataset")
-    content_images = UnlabelledImageDataset("MiniCOCO/256/")
-    style_images = UnlabelledImageListDataset(style_path, transform=Compose(
-        [
-            Resize(512),
-            RandomCrop((256, 256))
-        ]
-    ))
 
+    # content_images = UnlabelledImageDataset("MiniCOCO/256/")
+    # style_images = UnlabelledImageListDataset(style_path, transform=Compose(
+    #     [
+    #         Resize(512),
+    #         RandomCrop((256, 256))
+    #     ]
+    # ))
 
-    # img_dim = (128, 128)
-    # print("Begin creating dataset")
-    # content_images = UnlabelledImageDataset("MiniCOCO/128/", img_dim=img_dim)
-    # style_images = UnlabelledImageDataset(style_path, img_dim=img_dim)
+    img_dim = (128, 128)
+    content_images = UnlabelledImageDataset("MiniCOCO/128/", img_dim=img_dim)
+    style_images = UnlabelledImageDataset(style_path, img_dim=img_dim)
 
 
     # content_images = UnlabelledImageListDataset("data/", img_dim=img_dim)
